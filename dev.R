c6642e07529010819f217ce3687533903daca992
Phileas Condemine <phileas.condemine@gmail.com>
2022-02-28 16:36:48 +0100

    back to rmd render_site

--- dev.R
+++ dev.R
@@ -2,13 +2,16 @@ usethis::use_git()
 
 usethis::use_mit_license("Marine Olivari")
 
-rmarkdown::render("index.Rmd")
+# rmarkdown::render("index.Rmd")
+rmarkdown::clean_site(preview = FALSE)
+rmarkdown::render_site()
+# shell("rename _site docs")# rename drectory _site/ to docs
 
 
 system('git config --global user.email "phileas.condemine@gmail.com"')
 system('git config --global user.name "Phileas Condemine"')
 
 system("git add .")
-system('git commit -m "rmdformats::material not mobile !"')
+# system('git commit -m "rmdformats::material not mobile !"')rmdformats::material
 system(sprintf('git commit -m "update %s"',Sys.Date()))
 system("git push origin master")
