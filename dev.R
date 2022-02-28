190f07bf392dc77e22b953e4121d48c41b5d9ff1
Phileas Condemine <phileas.condemine@gmail.com>
2022-02-28 14:02:06 +0100

    integration github pages

--- dev.R
+++ dev.R
@@ -4,3 +4,12 @@ usethis::use_mit_license("Marine Olivari")
 
 rmarkdown::render_site()
 
+shell("rename _site docs")# rename directory _site/ to docs
+
+
+system('git config --global user.email "phileas.condemine@gmail.com"')
+system('git config --global user.name "Phileas Condemine"')
+
+system("git add .")
+system(sprintf('git commit -m "update %s"',Sys.Date()))
+system("git push origin master")
